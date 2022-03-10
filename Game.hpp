c5c15e3dfe31a31450cf710f9135e92aa5a8b171
zoonose99 <zoonose99@gmail.com>
2022-03-10 05:13:03 -0500

    game header

---
+++ Game.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifndef Game_hpp
+#define Game_hpp
+
+#include "SDL.h"
+#include <iostream>
+
+class Game {
+
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+
+	bool running() { return isRunning; }; //inline
+
+private:
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
+
+
+#endif /*Game_hpp */
