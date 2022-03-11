b89ebee53cf681eeae61bb366da8efd0a4a5af93
zoonose99 <zoonose99@gmail.com>
2022-03-10 21:21:19 -0500

    game object class

---
+++ GameObject.cpp
@@ -0,0 +1,29 @@
+#include "GameObject.h"
+#include "TextureManager.h"
+
+GameObject::GameObject(const char* texturesheet, SDL_Renderer* ren)
+{
+	renderer = ren;
+	objTexture = TextureManager::LoadTexture(texturesheet, ren);
+}
+
+void GameObject::Update()
+{
+	xpos = 0;
+	ypos = 0; 
+
+	srcRect.h = 64; 
+	srcRect.w = 32;
+	srcRect.x = 0;
+	srcRect.y = 0;
+
+	destRect.x = xpos;
+	destRect.y = ypos; 
+	destRect.w = srcRect.w * 2;
+	destRect.h = srcRect.h * 2;
+}
+
+void GameObject::Render()
+{
+	SDL_RenderCopy(renderer, objTexture, &srcRect, &destRect); 
+}
